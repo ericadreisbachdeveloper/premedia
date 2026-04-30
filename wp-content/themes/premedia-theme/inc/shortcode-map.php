8a3c3a6b9acc3a9c95bb2a19fcc0d97f50d55108
erica dreisbach <erica@peppergroup.com>
2026-04-30 11:07:41 -0500

    feat: Map controls only on laptop-and-wider and pointer: fine (not hi-res iPad)

--- wp-content/themes/premedia-theme/inc/shortcode-map.php
+++ wp-content/themes/premedia-theme/inc/shortcode-map.php
@@ -30,7 +30,7 @@ function map_shortcode_fxn()
 
 
     // Map controls
-    $map_output .= '<div class="map-controls tablet-hide" role="group" aria-label="Map controls">';
+    $map_output .= '<div class="map-controls" role="group" aria-label="Map controls">';
     $map_output .= '<div class="map-zoom">
     <button class="map-control-btn" id="map-zoom-in" aria-label="Zoom in"><svg class="map-zoom-in-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 160-160 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l160 0 0 160c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160 160 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-160 0 0-160z"/></svg></button>';
     $map_output .= '<button class="map-control-btn" id="map-zoom-out" aria-label="Zoom out"><svg class="map-zoom-out-svg"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32z"/></svg></button>
